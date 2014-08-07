063a40a59cd66934b1d68474a5d2f3794d3145f0
Xshl5 <xshl5@163.com>
2014-08-07 11:51:31 +0800

    Added libminiupnpc supports.

--- jni/Android.mk
+++ jni/Android.mk
@@ -10,6 +10,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmp3encoder
 LOCAL_CFLAGS := -O2 -Wall -DANDROID -DSTDC_HEADERS -I./libmp3lame/ 
+LOCAL_ARM_MODE := arm
 
 #including source files
 include $(LOCAL_PATH)/libmp3lame_build.mk
@@ -24,6 +25,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libnatpmp
 LOCAL_CFLAGS := -O2 -Wall -DANDROID -DLINUX -I./libnatpmp/  
+LOCAL_ARM_MODE := arm
 
 #including source files
 include $(LOCAL_PATH)/libnatpmp_build.mk
@@ -31,3 +33,31 @@ include $(LOCAL_PATH)/libnatpmp_build.mk
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)   
+
+###########################################################
+#   uPnPc port mapping
+#
+include $(CLEAR_VARS)
+LOCAL_MODULE := libminiupnpc
+LOCAL_CFLAGS := -O2 -Wall -DANDROID -DLINUX -I./libminiupnpc/
+LOCAL_ARM_MODE := arm
+
+#including source files
+LOCAL_SRC_FILES += libminiupnpc/miniwget.c \
+                   libminiupnpc/minixml.c \
+                   libminiupnpc/igd_desc_parse.c \
+                   libminiupnpc/minisoap.c \
+                   libminiupnpc/miniupnpc.c \
+                   libminiupnpc/upnpreplyparse.c \
+                   libminiupnpc/upnpcommands.c \
+                   libminiupnpc/upnperrors.c \
+                   libminiupnpc/connecthostport.c \
+                   libminiupnpc/portlistingparse.c \
+                   libminiupnpc/receivedata.c \
+                   libminiupnpc/minissdpc.c \
+                   libminiupnpc/upnpc.c \
+                   miniupnpc.cpp
+
+LOCAL_LDLIBS := -llog
+
+include $(BUILD_SHARED_LIBRARY)
